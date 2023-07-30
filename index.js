9ccaa20179d94d1e3edc4bc36cce8a30762eed39
Maria Elena <mariaelenamassarelli@gmail.com>
2023-07-30 14:48:37 +0100

    Descriptions movies updated

--- index.js
+++ index.js
@@ -11,19 +11,22 @@ app.use(bodyParser.json());
 
 let users = [
   {
-    id: 1,
-    name: "Kim",
-    favoriteMovies: [],
+    userid: 1,
+    username: "Kim",
+    email: "kim@gmail.com",
+    favoriteMovies: "Home Alone",
   },
   {
-    id: 2,
-    name: "Joe",
-    favoriteMovies: ["Maleficent"],
+    userid: 2,
+    username: "Joe",
+    email: "joe@gmail.com",
+    favoriteMovies: "Maleficent",
   },
   {
-    id: 3,
-    name: "James",
-    favoriteMovies: ["Harry Potter and the Philosopher's Stone"],
+    userid: 3,
+    username: "James",
+    email: "james@gmail.com",
+    favoriteMovies: "Harry Potter and the Philosopher's Stone",
   },
 ];
 
@@ -41,17 +44,33 @@ let movies =
         },
         {
           Name: "Adventure",
-          Description: "Adventure movies contain many of the same features of action movies, but are usually set in exotic locations. The main theme is adventure, with the characters often exploring places they have not been before or doing things they have not done before. (Source: Wikipedia)"
+          Description: "Adventure movies contain many of the same features of action movies, but are usually set in exotic locations. The main theme is adventure, with the characters often exploring places they have not been before or doing things they have not done before. (Source: Wikipedia)",
         }
       ],
+    Description: "An 11-year-old orphan living with his unwelcoming aunt, uncle, and cousin, who learns of his own fame as a wizard known to have survived his parents' murder at the hands of the dark wizard Lord Voldemort as an infant when he is accepted to Hogwarts School of Witchcraft and Wizardry. (Source: Wikipedia)",
     Directors: 
-      [
         { Name: "Chris Columbus",
-          Birth: "1958",
-          Death: "-",
-          Bio: "Chris Joseph Columbus is an American filmmaker. Born in Spangler, Pennsylvania, Columbus studied film at Tisch School of the Arts where he developed an interest in filmmaking. (Source: Wikipedia)"
+          Birthyear: "1958",
+          Deathyear: "-",
+          Bio: "Chris Joseph Columbus is an American filmmaker. Born in Spangler, Pennsylvania, Columbus studied film at Tisch School of the Arts where he developed an interest in filmmaking. (Source: Wikipedia)",
         },
-      ],
+  },
+  {
+    Title: "Home Alone",
+    Year: "1990",
+    Genre:
+        {
+          Name: "Comedy",
+          Description: "Comedy is a genre of fiction that consists of discourses or works intended to be humorous or amusing by inducing laughter. (Source: Wikipedia)",
+        },
+    Description: "An eight-year-old troublemaker, mistakenly left home alone, must defend his home against a pair of burglars on Christmas eve. (Source: imdb.com)",
+    Directors: 
+        { 
+          Name: "Chris Columbus",
+          Birthyear: "1958",
+          Deathyear: "-",
+          Bio: "Chris Joseph Columbus is an American filmmaker. Born in Spangler, Pennsylvania, Columbus studied film at Tisch School of the Arts where he developed an interest in filmmaking. (Source: Wikipedia)",
+        },
   },
   {
     Title: "Maleficent",
@@ -64,17 +83,17 @@ let movies =
         },
         {
           Name: "Adventure",
-          Description: "Adventure movies contain many of the same features of action movies, but are usually set in exotic locations. The main theme is adventure, with the characters often exploring places they have not been before or doing things they have not done before. (Source: Wikipedia)"
+          Description: "Adventure movies contain many of the same features of action movies, but are usually set in exotic locations. The main theme is adventure, with the characters often exploring places they have not been before or doing things they have not done before. (Source: Wikipedia)",
         }
       ],
+    Description: "Maleficent is a kind-hearted fairy, who is deceived by the love of her life, Stefan. Soon, she places a curse on his daughter, Aurora, in order to exact revenge. (Source: Wikipedia)",
     Directors: 
-      [
-        { Name: "Robert Stromberg", 
-          Birth: "1965",
-          Death: "-",
-          Bio: "Robert Stromberg is an American special effects artist, designer and filmmaker. Stromberg's credits include films such as James Cameron's Avatar, Tim Burton's Alice in Wonderland, and Sam Raimi's Oz the Great and Powerful. (Source: Wikipedia)"
+        { 
+          Name: "Robert Stromberg", 
+          Birthyear: "1965",
+          Deathyear: "-",
+          Bio: "Robert Stromberg is an American special effects artist, designer and filmmaker. Stromberg's credits include films such as James Cameron's Avatar, Tim Burton's Alice in Wonderland, and Sam Raimi's Oz the Great and Powerful. (Source: Wikipedia)",
         },
-      ],
   },
   {
     Title: "Bridget Jones's Baby",
@@ -87,17 +106,17 @@ let movies =
         },
         {
           Name: "Romance",
-          Description: "Romance film can be defined as a genre wherein the plot revolves around the love between two protagonists. This genre usually has a theme that explores an issue within love, including but not limited to: love at first sight, forbidden love, love triangles, and sacrificial love. (Source: thescriptlab.com)"
+          Description: "Romance film can be defined as a genre wherein the plot revolves around the love between two protagonists. This genre usually has a theme that explores an issue within love, including but not limited to: love at first sight, forbidden love, love triangles, and sacrificial love. (Source: thescriptlab.com)",
         }
       ],
+    Description: "After breaking up with Mark, Bridget bumps into a handsome stranger, Jack. Soon, Bridget finds out that she is pregnant and unsure about the identity of her baby's father. (Source: Wikipedia)",
     Directors: 
-      [
-        { Name: "Sharon Maguire",
-          Birth: "1960",
-          Death: "-",
-          Bio: "Sharon Maguire is a film director best known for directing Bridget Jones's Diary. The film was based on the book by her close friend Helen Fielding, and one of the main characters, Shazza, is allegedly based on Maguire. (Source: Wikipedia)"
+        {
+          Name: "Sharon Maguire",
+          Birthyear: "1960",
+          Deathyear: "-",
+          Bio: "Sharon Maguire is a film director best known for directing Bridget Jones's Diary. The film was based on the book by her close friend Helen Fielding, and one of the main characters, Shazza, is allegedly based on Maguire. (Source: Wikipedia)",
         },
-      ],
   },
   {
     Title: "Fantastic Beasts: The Crimes of Grindelwald",
@@ -110,34 +129,34 @@ let movies =
         },
         {
           Name: "Adventure",
-          Description: "Adventure movies contain many of the same features of action movies, but are usually set in exotic locations. The main theme is adventure, with the characters often exploring places they have not been before or doing things they have not done before. (Source: Wikipedia)"
+          Description: "Adventure movies contain many of the same features of action movies, but are usually set in exotic locations. The main theme is adventure, with the characters often exploring places they have not been before or doing things they have not done before. (Source: Wikipedia)",
         }
       ],
+    Description: "Gellert Grindelwald plans to raise an army of wizards to rule over non-magical beings. In response, Newt Scamander's former professor, Albus Dumbledore, seeks his help to stop him. (Source: Wikipedia)",
     Directors: 
-      [
-        { Name: "David Yates", 
-          Birth: "1963",
-          Death: "-",
-          Bio: "David Yates is an English film director, producer and screenwriter, who has directed feature films, short films, and television productions. He is best known for directing the final four films in the Harry Potter series and the three films of its prequel series, Fantastic Beasts. (Source: Wikipedia)"
+        { 
+          Name: "David Yates", 
+          Birthyear: "1963",
+          Deathyear: "-",
+          Bio: "David Yates is an English film director, producer and screenwriter, who has directed feature films, short films, and television productions. He is best known for directing the final four films in the Harry Potter series and the three films of its prequel series, Fantastic Beasts. (Source: Wikipedia)",
         },
-      ], 
   },
   {
     Title: "Casper",
     Year: "1995",
     Genre: 
-      {
-        Name: "Fantasy",
-        Description: "Fantasy is a genre that features magical and supernatural elements that do not exist in the real world. (Source: bookriot.com)",
-      },
-    Directors: 
-      [
-        { Name: "Brad Silberling",
-          Birth: "1963",
-          Death: "-",
-          Bio: "Bradley Mitchell Silberling is an American television and film director whose credits include the feature films Casper, City of Angels, Moonlight Mile, Lemony Snicket's A Series of Unfortunate Events and Land of the Lost. (Source: Wikipedia)"
+        {
+          Name: "Fantasy",
+          Description: "Fantasy is a genre that features magical and supernatural elements that do not exist in the real world. (Source: bookriot.com)",
         },
-      ],
+    Description: "An afterlife therapist and his daughter meet a friendly young ghost when they move into a crumbling mansion in order to rid the premises of wicked spirits. (Source: imdb.com)",
+    Directors: 
+        { 
+          Name: "Brad Silberling",
+          Birthyear: "1963",
+          Deathyear: "-",
+          Bio: "Bradley Mitchell Silberling is an American television and film director whose credits include the feature films Casper, City of Angels, Moonlight Mile, Lemony Snicket's A Series of Unfortunate Events and Land of the Lost. (Source: Wikipedia)",
+        },
   },
   {
     Title: "Jumanji",
@@ -150,44 +169,51 @@ let movies =
         },
         {
           Name: "Adventure",
-          Description: "Adventure movies contain many of the same features of action movies, but are usually set in exotic locations. The main theme is adventure, with the characters often exploring places they have not been before or doing things they have not done before. (Source: Wikipedia)"
+          Description: "Adventure movies contain many of the same features of action movies, but are usually set in exotic locations. The main theme is adventure, with the characters often exploring places they have not been before or doing things they have not done before. (Source: Wikipedia)",
         }
       ],
+    Description: "Two children come across a magical board game. While playing it, they meet Alan, a man who was trapped in the game, and attempt to free him while facing different kinds of danger. (Source: Wikipedia)",
     Directors: 
-      [
-        { Name: "Joe Johnston",
-          Birth: "1950",
-          Death: "-",
-          Bio: "Joe Johnston is an American film director, producer, writer, and visual effects artist. He is best known for directing effects-driven films, including Honey, I Shrunk the Kids; The Rocketeer; Jumanji; Jurassic Park III; The Wolfman; and Captain America: The First Avenger. (Source: Wikipedia)"
+        { 
+          Name: "Joe Johnston",
+          Birthyear: "1950",
+          Deathyear: "-",
+          Bio: "Joe Johnston is an American film director, producer, writer, and visual effects artist. He is best known for directing effects-driven films, including Honey, I Shrunk the Kids; The Rocketeer; Jumanji; Jurassic Park III; The Wolfman; and Captain America: The First Avenger. (Source: Wikipedia)",
         },
-      ],
   },
   {
     Title: "Star Wars: Episode II - Attack of the Clones",
     Year: "2002",
     Genre: 
-      {
-        Name: "Sci-Fi",
-        Description: "A form of fiction that deals principally with the impact of actual or imagined science upon society or individuals. (Source: britannica.com",
-      },
-    Directors: 
-      [
-        { Name: "George Lucas",
-          Birth: "1944",
-          Death: "-",
-          Bio: "George Walton Lucas Jr. is an American filmmaker. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, LucasArts, Industrial Light & Magic and THX. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012. (Source: Wikipedia)"
+        {
+          Name: "Sci-Fi",
+          Description: "A form of fiction that deals principally with the impact of actual or imagined science upon society or individuals. (Source: britannica.com",
         },
-      ]
+    Description: "While pursuing an assassin, Obi Wan uncovers a sinister plot to destroy the Republic. With the fate of the galaxy hanging in the balance, the Jedi must defend the galaxy against the evil Sith. (Source: Wikipedia)",
+    Directors: 
+        { 
+          Name: "George Lucas",
+          Birthyear: "1944",
+          Deathyear: "-",
+          Bio: "George Walton Lucas Jr. is an American filmmaker. Lucas is best known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, LucasArts, Industrial Light & Magic and THX. He served as chairman of Lucasfilm before selling it to The Walt Disney Company in 2012. (Source: Wikipedia)",
+        },
   },
   {
     Title: "Pride and prejudice",
     Year: "2005",
     Genre: 
-      {
-        Name: "Romance",
-        Description: "Romance film can be defined as a genre wherein the plot revolves around the love between two protagonists. This genre usually has a theme that explores an issue within love, including but not limited to: love at first sight, forbidden love, love triangles, and sacrificial love. (Source: thescriptlab.com)"
-      },
-    Directors: { Name: "Joe Wright" },
+        {
+          Name: "Romance",
+          Description: "Romance film can be defined as a genre wherein the plot revolves around the love between two protagonists. This genre usually has a theme that explores an issue within love, including but not limited to: love at first sight, forbidden love, love triangles, and sacrificial love. (Source: thescriptlab.com)",
+        },
+    Description: "Mrs Bennet insists that her daughters find rich husbands and settle down. When a wealthy bachelor starts living near them, Mrs Bennet's happiness knows no bounds. (Source: Wikipedia)",
+    Directors: 
+        { 
+          Name: "Joe Wright",
+          Birthyear: "1972",
+          Deathyear: "-",
+          Bio: "Joseph Wright is an English film director residing in Somerset, England. His motion pictures include the literary adaptations Pride & Prejudice (2005), Atonement (2007), Anna Karenina (2012), and Cyrano (2021), the action thriller Hanna (2011), Peter Pan origin story Pan (2015), and Darkest Hour (2017), a political drama following Winston Churchill during World War II nominated for Best Picture. (Source: Wikipedia)",
+        },
   },
   {
     Title: "The mask",
@@ -203,32 +229,31 @@ let movies =
           Description: "Comedy is a genre of fiction that consists of discourses or works intended to be humorous or amusing by inducing laughter. (Source: Wikipedia)",
         },
       ],
+    Description: "Stanley, a meek bank employee, turns into an eccentric and maniacal green-skinned superhero who can bend reality, after wearing a wooden mask that was created by Loki, the Norse god of mischief. (Source: Wikipedia)",
     Directors: 
-      [
-        { Name: "Chuck Russell",
-          Birth: "1958",
-          Death: "-",
-          Bio: "Charles Russell is an American filmmaker and actor known for his work on several genre films. Some of Russell's best known films include the slasher fantasy film A Nightmare on Elm Street 3: Dream Warriors, the 1988 remake[1] of the 1958 monster horror film The Blob, the fantasy superhero comedy film The Mask, the action film Eraser, and the action-adventure The Scorpion King. (Source: Wikipedia)"
+        { 
+          Name: "Charles Russell",
+          Birthyear: "1958",
+          Deathyear: "-",
+          Bio: "Charles Russell is an American filmmaker and actor known for his work on several genre films. Some of Russell's best known films include the slasher fantasy film A Nightmare on Elm Street 3: Dream Warriors, the 1988 remake of the 1958 monster horror film The Blob, the fantasy superhero comedy film The Mask, the action film Eraser, and the action-adventure The Scorpion King. (Source: Wikipedia)",
         },
-      ]
-   
   },
   {
     Title: "Edward Scissorhands",
     Year: "1990",
     Genre: 
-      {
-        Name: "Fantasy",
-        Description: "Fantasy is a genre that features magical and supernatural elements that do not exist in the real world. (Source: bookriot.com)",
-      },
-    Directors: 
-      [
-        { Name: "Tim Burton",
-          Birth: "1958",
-          Death: "-",
-          Bio: "Timothy Walter Burton is an American filmmaker, animator, and artist. He is known for his gothic fantasy and horror films such as Beetlejuice, Edward Scissorhands, The Nightmare Before Christmas, and Dark Shadows, as well as the television series Wednesday (2022). (Source: Wikipedia)"
+        {
+          Name: "Fantasy",
+          Description: "Fantasy is a genre that features magical and supernatural elements that do not exist in the real world. (Source: bookriot.com)",
         },
-      ]
+    Description:"Edward, a synthetic man with scissor hands, is taken in by Peg, a kindly Avon lady, after the passing of his inventor. Things take a turn for the worse when he is blamed for a crime he did not commit. (Source: Wikipedia)",
+    Directors: 
+        { 
+          Name: "Tim Burton",
+          Birthyear: "1958",
+          Deathyear: "-",
+          Bio: "Timothy Walter Burton is an American filmmaker, animator, and artist. He is known for his gothic fantasy and horror films such as Beetlejuice, Edward Scissorhands, The Nightmare Before Christmas, and Dark Shadows, as well as the television series Wednesday (2022). (Source: Wikipedia)",
+        },
   },
 ];
 
